e5795986a2091ebc4a9e87ee41c87f66d2c12f67
Jody Alkema <jody@alkema.ca>
2020-04-16 16:14:03 -0700

    Update graphs with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# AdminUser.create!(email: 'jody@alkema.ca', password: 'password', password_confirmation: 'password') if Rails.env.development?
+[
+  {
+    'serial_number': 'B082PZAZC05270R',
+    'firmware_version': '1.333.9999.9',
+    'registered_on': Date.today
+  },
+  {
+    'serial_number': 'B082PZAZC05270T',
+    'firmware_version': '1.333.9999.9',
+    'registered_on': Date.today
+  },
+  {
+    'serial_number': 'B082PZAZC05270N',
+    'firmware_version': '1.333.9999.9',
+    'registered_on': Date.today
+  },
+  {
+    'serial_number': 'B082PZAZC05270M',
+    'firmware_version': '1.333.9999.10',
+    'registered_on': Date.today
+  }
+].each do |device_atts|
+  Device.create!(device_atts)
+end
 
-device = Device.last
+def batch_create_readings(device)
+  start = 2.months.ago
+  stop = Time.zone.now
 
-start = 1.day.ago
-stop  = Time.zone.now
+  (start.to_i..stop.to_i).step(1.minute).each do |reading_at|
+    DeviceReading.create(
+      device: device,
+      temperature: 40.upto(50).to_a.sample,
+      humidity: 60.upto(80).to_a.sample,
+      carbon_monoxide: 1.upto(10).to_a.sample,
+      health_status: 'good',
+      reading_at: DateTime.strptime(reading_at.to_s,'%s')
+    )
+  end
+end
 
-(start.to_i..stop.to_i).step(1.minute).each do |reading_at|
-  DeviceReading.create(
-    device: device,
-    temperature: 40.upto(50).to_a.sample,
-    humidity: 20.upto(80).to_a.sample,
-    carbon_monoxide: 1.upto(10).to_a.sample,
-    health_status: ['fair', 'good', 'bad'].sample,
-    reading_at: DateTime.strptime(reading_at.to_s,'%s')
-  )
+Device.all.each do |device|
+  batch_create_readings(device)
 end
