f678d762a85b670d2efc1b65cd05423c840f750b
Jody Alkema <jody@alkema.ca>
2020-04-15 20:11:56 -0700

    Clean up some cruft

--- app/admin/device_readings.rb
+++ app/admin/device_readings.rb
@@ -2,24 +2,10 @@ ActiveAdmin.register DeviceReading do
 
   scope :all, default: true
 
-  # assumes the model has a scope called ':active'
   scope :this_week
   scope :this_month
   scope :this_year
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
   permit_params :device_id, :temperature, :humidity, :carbon_monoxide, :health_status, :reading_at
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:device_id, :temperature, :humidity, :carbon_monoxide, :health_status, :reading_at]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
 
 end
--- app/admin/devices.rb
+++ app/admin/devices.rb
@@ -1,19 +1,6 @@
 ActiveAdmin.register Device do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
   permit_params :serial_number, :firmware_version, :registered_on
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:serial_number, :firmware_version, :registered_on]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
 
   show do
     render 'charts', { device: device }
